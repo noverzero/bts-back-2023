77418b276c1dff1ee9effc4959d72116c7d90115
dustinhuth <dustin@undefinedindustries.com>
2019-01-28 18:25:47 -0700

    added headliner bio and headliner img link to events.

--- app.js
+++ app.js
@@ -24,7 +24,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-//app.use('/', indexRouter);
 app.use('/events', eventsRouter);
 app.use('/pickup_locations', pickupLocationsRouter);
 app.use('/users', usersRouter);
