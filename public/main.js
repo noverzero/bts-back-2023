8d22ce6829093764b35965a91c0b59a72428c986
Jake Mosher <jakemo136@gmail.com>
2019-01-31 09:42:12 -0700

    initial commit for Jake

--- public/main.js
+++ public/main.js
@@ -1,5 +1,5 @@
-import Schedule from 'node-schedule'
+// import Schedule from 'node-schedule'
 
-var j = Schedule.scheduleJob('1 * * * *', function(){
-  console.log('The answer to life, the universe, and everything!');
-});
+// var j = Schedule.scheduleJob('1 * * * *', function(){
+//   console.log('The answer to life, the universe, and everything!');
+// });
