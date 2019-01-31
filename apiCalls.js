7d97e0de4d27b73b886330a14eefba7993d32464
Jake Mosher <jakemo136@gmail.com>
2019-01-31 09:49:30 -0700

    test code for cron task scheduler

--- apiCalls.js
+++ apiCalls.js
@@ -1,7 +1,14 @@
-// var schedule = require('node-schedule');
-//
-// var j = schedule.scheduleJob('42 * * * *', function(){
-//   console.log('The answer to life, the universe, and everything!');
-// });
-
 // http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=Cher&api_key=bb5f39887cc93aa41c362ba1b8bbaccd&format=json
+
+const pingSongKick = () => {
+  console.log('ping songkick!')
+}  
+// const response = await fetch(`https://api.songkick.com/api/3.0/venues/591/calendar.json?per_page=100&apikey=8ViJ6NJZPEwjp3Cp`)
+// const json = await response.json()
+// const shows = json.resultsPage.results.event
+
+
+// newState.shows.map(show => show.displayName = show.displayName.split(’ at Red Rocks’)[0])
+// newState.shows.map(show => show.venue.displayName = show.venue.displayName.split(' Amphitheatre’)[0])
+// newState.shows.map(show => show.start.date = show.start.date.split(‘-’).splice(1, 3).concat(show.start.date.split(‘-’)[0]).join(‘/’))
+module.exports = pingSongKick
