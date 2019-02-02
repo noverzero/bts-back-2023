6a3a87dab02d06cafccb816f316750954ef89aca
kziechmann <kziechmann@gmail.com>
2019-02-02 12:11:06 -0700

    Created working post route for stripe charge in orders router

--- app.js
+++ app.js
@@ -1,20 +1,15 @@
+
+if (process.env.NODE_ENV !== 'production'){
+  require('dotenv').load();
+}
+
+var stripeSecretKey = process.env.STRIPE_SECRETKEY;
+var stripePublicKey = process.env.STRIPE_PUBLICKEY;
+var stripe = require('stripe')(stripeSecretKey);
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var dotenv = require('dotenv').config()
-var schedule = require('node-schedule')
-
-//var indexRouter = require('./routes/index');
-var eventsRouter = require('./routes/events');
-var pickupLocationsRouter = require('./routes/pickup_locations');
-var usersRouter = require('./routes/users');
-var discountCodesRouter = require('./routes/discount_codes');
-var ordersRouter = require('./routes/orders');
-var pickupPartiesRouter = require('./routes/pickup_parties');
-
-
-
 
 var app = express();
 app.use(function(req, res, next) {
@@ -29,12 +24,23 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+var schedule = require('node-schedule')
+
+//var indexRouter = require('./routes/index');
+var eventsRouter = require('./routes/events');
+var pickupLocationsRouter = require('./routes/pickup_locations');
+var usersRouter = require('./routes/users');
+var discountCodesRouter = require('./routes/discount_codes');
+var ordersRouter = require('./routes/orders');
+var pickupPartiesRouter = require('./routes/pickup_parties');
+
 app.use('/events', eventsRouter);
 app.use('/pickup_locations', pickupLocationsRouter);
 app.use('/users', usersRouter);
 app.use('/discount_codes', discountCodesRouter);
 app.use('/orders', ordersRouter);
 app.use('/pickup_parties', pickupPartiesRouter);
+
 
 app.use(function(req, res) {
   res.status(404).send('Not Found');
--- routes/orders.js
+++ routes/orders.js
@@ -3,57 +3,102 @@
 const express = require('express');
 const router = express.Router();
 const knex = require('../knex.js')
+var stripeSecretKey = process.env.STRIPE_SECRETKEY;
+var stripePublicKey = process.env.STRIPE_PUBLICKEY;
+const stripe = require('stripe')(stripeSecretKey);
 
 
 //List (get all of the resource)
-router.get('/', function(req, res, next){
-knex('orders')
-.select('id', 'pickupLocationId', 'eventId', 'reservationId', 'reservationWillCallName', 'discountCodeId', 'status')
-.then((data) => {
-res.status(200).json(data)
-  })
+router.get('/', function (req, res, next) {
+  knex('orders')
+    .select('id', 'pickupLocationId', 'eventId', 'reservationId', 'reservationWillCallName', 'discountCodeId', 'status')
+    .then((data) => {
+      res.status(200).json(data)
+    })
 })
 
 //Read (get one of the resource)
 // Get One
-router.get('/:id', function(req, res, next){
-knex('orders')
-.select('id', 'pickupLocationId', 'eventId', 'reservationId', 'reservationWillCallName', 'discountCodeId', 'status')
-.where('id', req.params.id)
-.then((data) => {
-  res.status(200).json(data[0])
-})
+router.get('/:id', function (req, res, next) {
+  knex('orders')
+    .select('id', 'pickupLocationId', 'eventId', 'reservationId', 'reservationWillCallName', 'discountCodeId', 'status')
+    .where('id', req.params.id)
+    .then((data) => {
+      res.status(200).json(data[0])
+    })
 })
 
 //Create (create one of the resource)
-router.post('/', function(req, res, next){
-// use req.body
-knex('orders')
-.insert(req.body)
-.returning(['id', 'pickupLocationId', 'eventId', 'reservationId', 'reservationWillCallName', 'discountCodeId', 'status'])
-.then((data) => {
-  res.status(200).json(data[0])
-})
+router.post('/', function (req, res, next) {
+  // use req.body
+  knex('orders')
+    .insert(req.body)
+    .returning(['id', 'pickupLocationId', 'eventId', 'reservationId', 'reservationWillCallName', 'discountCodeId', 'status'])
+    .then((data) => {
+      res.status(200).json(data[0])
+    })
 })
 
-router.patch('/:id', function(req, res, next){
-knex('orders')
-.where('id', req.params.id)
-.update(req.body)
-.returning(['id', 'pickupLocationId', 'eventId', 'reservationId', 'reservationWillCallName', 'discountCodeId', 'status'])
-.then((data) => {
-  res.status(200).json(data[0])
-})
+router.patch('/:id', function (req, res, next) {
+  knex('orders')
+    .where('id', req.params.id)
+    .update(req.body)
+    .returning(['id', 'pickupLocationId', 'eventId', 'reservationId', 'reservationWillCallName', 'discountCodeId', 'status'])
+    .then((data) => {
+      res.status(200).json(data[0])
+    })
 })
 
 //Delete (delete one of the resource)
-router.delete('/:id', function(req, res, next){
-knex('orders')
-.where('id', req.params.id)
-.del('*')
-.returning(['id', 'pickupLocationId', 'eventId', 'reservationId', 'reservationWillCallName', 'discountCodeId', 'status'])
-.then((data) => {
-  res.status(200).json(data[0])
+router.delete('/:id', function (req, res, next) {
+  knex('orders')
+    .where('id', req.params.id)
+    .del('*')
+    .returning(['id', 'pickupLocationId', 'eventId', 'reservationId', 'reservationWillCallName', 'discountCodeId', 'status'])
+    .then((data) => {
+      res.status(200).json(data[0])
+    })
 })
-})
+
+
+// router.post("/charge", async (req, res) => {
+//   try {
+//     // stripe.customers.create(JSON.stringify({
+//     //   email: req.body.stripeEmail,
+//     //   source: token
+//     // })).then(customer => {
+//       // return 
+//       stripe.charges.create({
+//         amount: req.body.totalCost,
+//         currency: "usd",
+//         description: "An example charge",
+//         source: token
+//       })
+//     // })
+//     .then(status => {
+//       res.json({
+//         ...status
+//       })
+//     })
+//   } catch (err) {
+//     res.status(500).end();
+//   }
+// });
+
+router.post('/charge', async(req, res) => {
+  const amount = 2500;
+  console.log(req.body)
+  stripe.customers.create({
+    email: req.body.stripeEmail,
+    source: req.body.stripeToken
+  })
+  .then(customer => stripe.charges.create({
+    amount,
+    description: 'example charge',
+    currency: 'usd',
+    customer: customer.id
+  }))
+  .then(charge => res.json(charge));
+});
+
 module.exports = router;
