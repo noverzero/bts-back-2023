b80c05e806d4fae745f6cb85e0c019b52283a117
dustinhuth <dustin@undefinedindustries.com>
2019-02-01 23:03:05 -0700

    good progress on discount codes route.  not finished, but not breaking anything.

--- routes/discount_codes.js
+++ routes/discount_codes.js
@@ -15,6 +15,20 @@ res.status(200).json(data)
   })
 })
 
+
+//VVVVVpractice joinVVVVV
+// router.get('/', function(req, res, next){
+//   knex('discount_codes')
+//   .join('discount_codes_events', 'discount_codes.id', 'discount_codes_events.discountCodeId')
+//   .join('events', 'discount_codes_events.eventsId', 'events.id')
+//   .select('*')
+//   .then((results) => {
+//     console.log('results', results)
+//     res.status(200).json(results)
+//   })
+//
+// })
+
 //Read (get one of the resource)
 // Get One
 router.get('/:id', function(req, res, next){
@@ -37,14 +51,81 @@ knex('discount_codes')
 })
 })
 
-router.patch('/:id', function(req, res, next){
+
+
+
+//if discount code is added to cart
+//for each discount code:
+//1) check to see if discount code exists in discount_codes
+      //if no, return error message
+      //if yes, go to step 2..
+//2) check if expiration date is earlier than today (expired)
+      //if no, return error message
+      // if yes, go to step 3
+//3) check number of remaining uses and store in accumulator variable.
+//4) check percentage discounted and store as variable
+
+// for each reservation
+
+///5) check to see if discount code is associated with event id (discountCode_event)
+      // if yes, go to step 5.
+//6) get base price before processing fee, then multiply price by (100 - percentage) mutliply result by 1.1 (processing fee)  and store in accumulator variable newPrice
+//7) decrement number of remaining uses.
+
+
+//return newTotal.
+//return number of remaining uses.
+
+
+// router.patch('/', function(req, res, next){
+// knex('pickup_parties')
+// .where({'pickupLocationId': req.body.pickupLocationId, 'eventId': req.body.eventId})
+// .increment('inCart', req.body.ticketQuantity)
+// .returning(['id', 'pickupLocationId', 'eventId', 'eventDate', 'eventVenue', 'lastBusDeparts', 'orderId', 'ordersReservationId', 'ordersWillCallName', 'checkedInPasscode', 'sold', 'capacity', 'inCart'])
+// .then((data) => {
+//   res.status(200).json(data[0])
+// })
+// })
+
+
+//
+
+router.patch('/:discountCode', function(req, res, next){
+  let discountCode = req.params.discountCode
 knex('discount_codes')
-.where('id', req.params.id)
-.update(req.body)
-.returning(['id', 'discountCode', 'percentage', 'expiresOn', 'issuedOn', 'issuedTo', 'issuedBy', 'issuedBecause', 'timesUsed', 'type', 'remainingUses', 'usesPerEvent'])
-.then((data) => {
-  res.status(200).json(data[0])
+.join('discount_codes_events', 'discount_codes.id', 'discount_codes_events.discountCodeId')
+.join('events', 'discount_codes_events.eventsId', 'events.id')
+.select('*')
+.where('discountCode', discountCode)
+.then((match) => {
+  if(!match){
+    next({status:400, message: 'Code not found.'})
+  }
+
+  let chicken = []
+  match.forEach( (matchElement, i) => {
+    let isExpired
+    if(matchElement.expiresOn.toLocaleString('en-US') < new Date().toLocaleString('en-US', {timeZone: 'America/Denver'})){
+      return "this code has expired"
+    }
+     let newObject={}
+      newObject.percentage=matchElement.percentage
+      newObject.remainingUses=matchElement.remainingUses
+
+      chicken.push(newObject)
+
+  })
+
+console.log('chicken:::::', chicken)
+
+
+
 })
+// .update(req.body)
+// .returning(['id', 'discountCode', 'percentage', 'expiresOn', 'issuedOn', 'issuedTo', 'issuedBy', 'issuedBecause', 'timesUsed', 'type', 'remainingUses', 'usesPerEvent'])
+// .then((data) => {
+//   res.status(200).json(data[0])
+// })
 })
 
 //Delete (delete one of the resource)
