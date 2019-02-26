6c2c39d3e756113fbaee998fbdc768db151ee6e6
dustinhuth <dustin@undefinedindustries.com>
2019-02-26 14:20:52 -0700

    npm installed and saved cors. app.use(cors()) in app.js.

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var logger = require('morgan');
 var dotenv = require('dotenv').config()
 var cron = require('node-cron')
 const ORIGIN_URL = process.env.ORIGIN_URL
+var cors = require('cors');
 
 var discountCodesEventsRouter = require('./routes/discount_codes_events')
 var discountCodesRouter = require('./routes/discount_codes');
@@ -22,12 +23,14 @@ var app = express();
 
 var reservationsRouter = require('./routes/reservations')
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://roomy-move.surge.sh/");
-  res.header("Access-Control-Allow-Methods", "GET,POST,DELETE,PATCH,PUT");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "http://roomy-move.surge.sh/");
+//   res.header("Access-Control-Allow-Methods", "GET,POST,DELETE,PATCH,PUT");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
+
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
