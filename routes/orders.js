ae4981b5f83dac97e42ef8931fa86d4e6e4eac24
dustinhuth <dustin@undefinedindustries.com>
2019-03-03 19:48:22 -0700

    testing email

--- routes/orders.js
+++ routes/orders.js
@@ -129,11 +129,13 @@ router.delete('/:id', function(req, res, next){
 })
 
 router.post('/charge', async(req, res) => {
+  console.log("req.body inside post /charge", req.body)
   stripe.customers.create({
     email: req.body.stripeEmail,
     source: req.body.stripeToken.id,
   })
   .then(customer =>{
+    console.log('customer inside post /charge', customer)
     stripe.charges.create({
         amount: req.body.amount,
         description: req.body.eventId,
