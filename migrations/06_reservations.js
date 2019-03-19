cfa4133d4b108933507b18e529f362cf06ab1ad9
Jake Mosher <jakemo136@gmail.com>
2019-03-19 15:48:32 -0600

    trying to clear the x

--- migrations/06_reservations.js
+++ migrations/06_reservations.js
@@ -7,12 +7,12 @@ exports.up = (knex, Promise) => {
     table.foreign('pickupPartiesId').references('pickup_parties.id')
     table.string('willCallFirstName').notNullable()
     table.string('willCallLastName').notNullable()
-    table.integer('status').notNullable().defaultTo(0)
+    table.integer('status').notNullable().defaultTo(1)
     table.integer('discountCodeId')
     table.foreign('discountCodeId').references('discount_codes.id')
   })
 }
-// status: 0 = reserved, 1 = checked in, 2 = refund/cancel
+// status: 1 = reserved, 2 = checked in, 3 = refund/cancel
 exports.down = (knex, Promise) => {
   return knex.schema.dropTable("reservations")
 }
---
+++ seeds/003_users.js
@@ -0,0 +1,33 @@
+const {util, seed} = require('data-seed')
+const firstNameSeed = () => { return seed.name.en.firstName() }
+const lastNameSeed = () => { return seed.name.en.lastName() }
+const seedEmail = () => { return seed.email() }
+const preferredLocationSeed = (arr) => { return  arr[Math.floor(Math.random() * arr.length)] }
+
+const generateUsersSeeds = (num) => {
+  let users = []
+  for (let i = 0; i < num; i++) {
+      users.push(
+        {firstName: firstNameSeed(), lastName: lastNameSeed(), email: seedEmail(), preferredLocation: preferredLocationSeed(['Denver', 'Boulder', 'Fort Collins', 'Longmont', '']), isStaff: false, isDriver: false, isAdmin: false, isDeactivated: false}
+      )
+  }
+  return users
+}
+const adminTest = { firstName: 'admin', lastName: 'test', email: 'admintest@nobody.com', isStaff: false, isDriver: false, isAdmin: true, isDeactivated: false }
+const staffTest = { firstName: 'staff', lastName: 'test', email: 'stafftest@nobody.com', isStaff: true, isDriver: false, isAdmin: false, isDeactivated: false }
+const driverTest = { firstName: 'driver', lastName: 'test', email: 'drivertest@nobody.com', isStaff: false, isDriver: true, isAdmin: false, isDeactivated: false }
+const deactivatedTest = { firstName: 'deactivated', lastName: 'test', email: 'deactivatedtest@nobody.com', isStaff: false, isDriver: false, isAdmin: false, isDeactivated: true }
+
+exports.seed = (knex) => {
+  // Deletes ALL existing entries
+  return knex('users').del()
+    .then(() => {
+      // Inserts seed entries
+      return knex('users').insert([...generateUsersSeeds(100), adminTest, staffTest, driverTest, deactivatedTest])
+    })
+    .then(() => {
+      return knex.raw("SELECT setval('users_id_seq', (SELECT MAX(id) FROM users))")
+    })
+}
+
+//user types = {standard, staff, driver, admin, deactivated}
