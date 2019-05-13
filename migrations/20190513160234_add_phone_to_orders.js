448e2bc96ff7441e77fd8f05fd4bd1d63551a03b
dustinhuth <dustin@undefinedindustries.com>
2019-05-13 16:25:49 -0600

    added phone column to orders migration file.

---
+++ migrations/20190513160234_add_phone_to_orders.js
@@ -0,0 +1,11 @@
+
+exports.up = (knex, Promise) => {
+  return knex.schema.table('orders', function(t) {
+      t.string('orderedByPhone');
+  });
+};
+
+exports.down = (knex, Promise) => {
+  return knex.schema.table('orders', function(t) {
+      t.dropColumn('orderedByPhone');
+  });
