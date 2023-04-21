ffeb89a97b8a1e60ef736ee6533eadd661e8941c
Dustin <dustin@undefinedindustries.com>
2023-04-21 02:00:43 -0600

    trying again

--- routes/users.js
+++ routes/users.js
@@ -78,64 +78,62 @@ router.post('/', function(req, res, next){
   const token = jwt.sign(payload, JWT_KEY, { expiresIn: '72h' });
   const email = req.body.email;
   const password = req.body.hshPwd;
-  bcrypt.genSalt(saltRounds, (err, salt) => {
+  let hshPass = bcrypt.genSalt(saltRounds, (err, salt) => {
     console.log('users/ route --- hash inside genSalt ==>>==>> ', req.body.hshPwd);
     //4068a95734305426ff1d81ee325cd4f9c9d5e382f4997f7fbea7450de0666016
-    bcrypt.hash(password, salt, (err, hash) => {
+    return bcrypt.hash(password, salt, (err, hash) => {
     // returns hash
-    req.body.hshPwd = hash.trim();
-    console.log('users/ route --- hash inside bcrypt.hshPwd ==>>==>> ', req.body.hshPwd);
+    return hash.trim();
     //$2b$10$UN1zGKdnjU/xQpDHz5P5Eu9EsoQOUlGb3Wb0teyR8Rq59JUwpOJti
-    console.log('req.body inside.then ==>>==>> ', req.body);
 
-    }).then(() => {
-      return knex('users')
-      .select('id', 'firstName', 'lastName', 'email', 'phone', 'isWaiverSigned', 'isStaff', 'isAdmin', 'isDriver', 'isDeactivated', 'preferredLocation')
-      .where('email', email)
-      .then((rows) =>{
-        if(rows.length===0){
-          return knex('users')
-          .insert(req.body)
-          .returning(['id', 'firstName', 'lastName', 'email', 'phone', 'isWaiverSigned', 'isStaff', 'isAdmin', 'isDriver', 'isDeactivated', 'preferredLocation'])
-          .then( (data) => {
-            sendRegistrationConfirmationEmail(email, 'confirm', token, origin);
-            res.status(200).json({
-              'message': 'email sent!',
-              'code': '200',
-              'email': `${email}`
-            })
-          }, (err) => {
-            res.status(500).json({
-              'message': 'email failed to send',
-              'code': '500',
-              'email': `${email}`
-            });
-          })
-        } else if(req.body.resendEmail === true){
-    
-          sendRegistrationConfirmationEmail(email, 'confirm', token, origin);
-          return res.status(200).json({
-            'message': 'email re-sent!',
-            'code': '200',
-            'email': `${email}`
-          })
-        } else {
-          res.status(200).json({
-            'message': 'account already exists',
-            'code': '202',
-            'email': `${email}`
-          }); 
-        }
-      })
-      .catch((err) => {
-        console.error(err);
-        res.status(500).json({ error: 'Failed to register user' });
-        next(err)
-      })
     });
-
   });
+  console.log('hshPass ==>>==>> ', hshPass);
+  req.body.hshPwd = hshPass;
+  console.log('users/ route --- req.body after genSalt ==>>==>> ', req.body);
+  return knex('users')
+  .select('id', 'firstName', 'lastName', 'email', 'phone', 'isWaiverSigned', 'isStaff', 'isAdmin', 'isDriver', 'isDeactivated', 'preferredLocation')
+  .where('email', email)
+  .then((rows) =>{
+    if(rows.length===0){
+      return knex('users')
+      .insert(req.body)
+      .returning(['id', 'firstName', 'lastName', 'email', 'phone', 'isWaiverSigned', 'isStaff', 'isAdmin', 'isDriver', 'isDeactivated', 'preferredLocation'])
+      .then( (data) => {
+        sendRegistrationConfirmationEmail(email, 'confirm', token, origin);
+        res.status(200).json({
+          'message': 'email sent!',
+          'code': '200',
+          'email': `${email}`
+        })
+      }, (err) => {
+        res.status(500).json({
+          'message': 'email failed to send',
+          'code': '500',
+          'email': `${email}`
+        });
+      })
+    } else if(req.body.resendEmail === true){
 
+      sendRegistrationConfirmationEmail(email, 'confirm', token, origin);
+      return res.status(200).json({
+        'message': 'email re-sent!',
+        'code': '200',
+        'email': `${email}`
+      })
+    } else {
+      res.status(200).json({
+        'message': 'account already exists',
+        'code': '202',
+        'email': `${email}`
+      }); 
+    }
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).json({ error: 'Failed to register user' });
+    next(err)
+  })
 })
 
 router.post('/login/', async (req, res) => {
